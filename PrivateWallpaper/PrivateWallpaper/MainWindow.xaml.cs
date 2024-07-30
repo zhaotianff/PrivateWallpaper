b6c8dec68ec858bcead1888c88288626f4480f84
zhaotianff <zhaotianff@163.com>
2024-07-30 15:30:29 +0800

    update ui

--- PrivateWallpaper/PrivateWallpaper/MainWindow.xaml.cs
+++ PrivateWallpaper/PrivateWallpaper/MainWindow.xaml.cs
@@ -9,6 +9,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -20,15 +21,48 @@ namespace PrivateWallpaper
     /// </summary>
     public partial class MainWindow : Window
     {
+        private Storyboard onAnimation;
+        private Storyboard offAnimation;
+
         public MainWindow()
         {
-            InitializeComponent();     
+            InitializeComponent();
+            onAnimation = FindResource("SwitchOnAnimation") as Storyboard;
+            offAnimation = FindResource("SwitchOffAnimation") as Storyboard;
         }
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
             this.Left = SystemParameters.PrimaryScreenWidth - 120;
             this.Top = SystemParameters.WorkArea.Height - 60;
+        }
+
+        private void Grid_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if(e.LeftButton == MouseButtonState.Pressed)
+            {
+                this.DragMove();
+            }
+        }
+
+        private async void PART_Ellipse_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if ((double)PART_Ellipse.GetValue(Canvas.LeftProperty) == 0)
+            {
+                onAnimation.Begin();
+                await Task.Delay(300);
+                BitmapImage bi = new BitmapImage();
+                bi.BeginInit();
+                bi.UriSource = new Uri("pack://application:,,,/PrivateWallpaper;component/Styles/back.jpg", UriKind.Absolute);
+                bi.EndInit();
+                this.PART_Border.Background = new ImageBrush() { ImageSource = bi, Stretch = Stretch.UniformToFill };
+            }
+            else
+            {
+                offAnimation.Begin();
+                this.PART_Border.Background = Brushes.White;
+            }
+
         }
     }
 }
