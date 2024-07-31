e042dbe253df69b80b032fa49ac0f8515c709b84
zhaotianff <zhaotianff@163.com>
2024-07-31 15:25:39 +0800

    one instance

---
+++ PrivateWallpaper/PrivateWallpaper/Startup.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+
+namespace WindowsX.Shell
+{
+    class Startup
+    {
+        private static readonly string MutexName = "MMutex";
+
+        [STAThread]
+        static void Main(string[] args)
+        {
+            bool createdNew;
+            System.Threading.Mutex mutex = new System.Threading.Mutex(true, MutexName, out createdNew);
+            if (createdNew)
+            {
+                PrivateWallpaper.App app = new PrivateWallpaper.App();
+                app.InitializeComponent();
+                app.Run();
+            }
+            else
+            {
+                //TODO activate window
+            }
+        }
+    }
+}
